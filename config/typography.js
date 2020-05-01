4eee3868cc9f52e0b7120330c70f4d7de0bb347d
Kate Gordon <kategordon.code@gmail.com>
2020-05-01 00:30:26 -0400

    initial commit, beginning to customize data

--- config/typography.js
+++ config/typography.js
@@ -1,37 +1,31 @@
-import Typography from 'typography'
+import Typography from "typography";
 
 // This website uses the system font stack after the placed "Lora" font
 // The scaleRatio will be overwritten for smaller breakpoints in src/components/Layout
 
 const typography = new Typography({
-  title: 'Gatsby Starter Prismic.io',
-  baseFontSize: '18px',
+  title: "Gatsby Starter Prismic.io",
+  baseFontSize: "18px",
   baseLineHeight: 1.45,
   headerFontFamily: [
-    'Lora',
-    '-apple-system',
-    'BlinkMacSystemFont',
-    'Segoe UI',
-    'Roboto',
-    'Helvetica',
-    'Arial',
-    'sans-serif',
-    'Apple Color Emoji',
-    'Segoe UI Emoji',
-    'Segoe UI Symbol',
+    "Roboto",
+    "sans-serif",
+    "Apple Color Emoji",
+    "Segoe UI Emoji",
+    "Segoe UI Symbol",
   ],
   bodyFontFamily: [
-    'Lora',
-    '-apple-system',
-    'BlinkMacSystemFont',
-    'Segoe UI',
-    'Roboto',
-    'Helvetica',
-    'Arial',
-    'sans-serif',
-    'Apple Color Emoji',
-    'Segoe UI Emoji',
-    'Segoe UI Symbol',
+    "Lora",
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "Segoe UI",
+    "Roboto",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+    "Apple Color Emoji",
+    "Segoe UI Emoji",
+    "Segoe UI Symbol",
   ],
   scaleRatio: 3.157,
   headerWeight: 700,
@@ -40,6 +34,6 @@ const typography = new Typography({
       marginBottom: 0,
     },
   }),
-})
+});
 
-export default typography
+export default typography;
