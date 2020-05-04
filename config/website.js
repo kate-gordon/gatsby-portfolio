c0da1637bf1f0049c8801fab9b5b6d76064e623a
Kate Gordon <kategordon.code@gmail.com>
2020-05-03 23:00:15 -0400

    added my own favicon to the starter

--- config/website.js
+++ config/website.js
@@ -11,7 +11,7 @@ module.exports = {
   ogLanguage: "en_US", // Facebook Language
 
   // JSONLD / Manifest
-  favicon: "/logos/katelogo.png", // Used for manifest favicon generation
+  favicon: "src/katelogo.png", // Used for manifest favicon generation
   shortName: "Prismic", // shortname for manifest. MUST be shorter than 12 characters
   author: "LekoArts", // Author for schemaORGJSONLD
   themeColor: "#3D63AE",
